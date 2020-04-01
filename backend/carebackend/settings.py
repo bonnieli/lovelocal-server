c39e5d0ebbbf1e1f8dddbbff3f573dfdcb820c23
Bonnie Li <bonnie@hatchways.io>
2020-04-01 11:27:16 -0400

    Docker file flex

--- backend/carebackend/settings.py
+++ backend/carebackend/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'lldtg$9(wi49j_hpv8nnqlh!cj7kmbwq0$rj7vy(b(b30vlyzj'
 
 # DEBUG = int(os.environ.get("DEBUG", default=0))
 DEBUG = False
